7de71417e2001393039a67989da1ffbff5f874c0
AlexandreLira <alexandrelira112@gmail.com>
2025-05-05 20:25:52 -0300

    feat: button and input component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import { Hero } from "./components/landing/hero";
 
 export default function Home() {
   return (
-    <div className="bg-background-primary">
-      <h1>Hello World</h1>
+    <div className="max-w-7xl mx-auto">
+      <h1 className="text-content-heading text-4xl font-bold">Hello World</h1>
+      {/* <Header /> */}
+      <Hero />
+      {/* <VideoExplanation /> */}
+      {/* <Pricing /> */}
+      {/* <FAQ /> */}
     </div>
   );
 }
